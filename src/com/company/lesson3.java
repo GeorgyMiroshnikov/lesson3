3e04cf62cba58e5948f33755b8862dbeeeecda82
GeorgyMiroshnikov <gmiroshnikov@meta-devs.io>
2022-01-04 18:21:59 +0500

    nubranch

--- src/com/company/lesson3.java
+++ src/com/company/lesson3.java
@@ -12,6 +12,7 @@ public class lesson3 {
 
             case 8:
                 System.out.println("число равно 8");
+                num++;
                 break;
 
             case 9:
@@ -19,7 +20,7 @@ public class lesson3 {
                 break;
 
             default:
-                System.out.println("число равно 1,8,9");
+                System.out.println("число не равно 1,8,9");
 
 
         }
